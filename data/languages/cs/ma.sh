cff4b0980767f820e27aa83073788935dbafed72
Martin Dočekal <docekmartin@gmail.com>
2021-02-18 11:47:48 +0100

    change of used ma

--- data/languages/cs/ma.sh
+++ data/languages/cs/ma.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/mnt/minerva1/nlp/projects/ma/xdocek09/ma/ma -C /mnt/minerva1/nlp/projects/ma/data/conf_fsa/czech_dict.conf $@
+/mnt/minerva1/nlp/projects/ma/xdocek09/ma/ma -C /mnt/minerva1/nlp/projects/entity_kb_czech_names/ma/czech_dict.conf $@
