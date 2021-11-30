009a6b68f991cf440dfa5c2cb724a7efe9d9ab4c
Martin Dočekal <docekmartin@gmail.com>
2021-11-30 13:43:23 +0100

    added sk language that is the same as cs

---
+++ data/languages/sk/eq_gen.py
@@ -0,0 +1,17 @@
+{
+    "P:::M": [
+        {"Sv.", "Sv", "St.", "sv.", "Svatí", "Svatý"},
+        {"Sr.", "sr.", "st.", "starší"},
+        {"Jr.", "jr.", "ml.", "mladší"}
+    ],
+    "P:::F": [
+        {"Sv.", "Sv", "St.", "sv.", "Svatá", "Svaté"},
+        {"Sr.", "sr.", "st.", "starší"},
+        {"Jr.", "jr.", "ml.", "mladší"}
+    ],
+    "L": [
+        {"Sv.", "Sv", "St.", "sv.", "Svatá", "Svaté", "Svatí", "Svatý"},
+        {"Sr.", "sr.", "st.", "starší"},
+        {"Jr.", "jr.", "ml.", "mladší"}
+    ],
+}
---
+++ data/languages/sk/ma.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/mnt/minerva1/nlp/projects/ma/ma_src/ma/ma -C /mnt/minerva1/nlp/projects/ma/data/conf_fsa/slovak_dict.conf $@
